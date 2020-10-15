a71df0f84664268e348c1e245775869068949627
hjyanghj <hjyanghj@gmail.com>
2020-10-16 02:19:19 +0800

    Ticket: Get Comments

--- M220N/Repositories/MoviesRepository.cs
+++ M220N/Repositories/MoviesRepository.cs
@@ -86,10 +86,16 @@ namespace M220N.Repositories
             {
                 return await _moviesCollection.Aggregate()
                     .Match(Builders<Movie>.Filter.Eq(x => x.Id, movieId))
-                    // Ticket: Get Comments
-                    // Add a lookup stage that includes the
-                    // comments associated with the retrieved movie
-                    .FirstOrDefaultAsync(cancellationToken);
+                // Ticket: Get Comments
+                // Add a lookup stage that includes the
+                // comments associated with the retrieved movie
+                .Lookup(
+                    _commentsCollection,
+                    m => m.Id,
+                    c => c.MovieId,
+                    (Movie m) => m.Comments
+                    )
+                .FirstOrDefaultAsync(cancellationToken);
             }
 
             catch (Exception ex)
